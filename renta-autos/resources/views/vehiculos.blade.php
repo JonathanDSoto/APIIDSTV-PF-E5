3928b6922da43efedff10aca8d596c880759aecf
RicardoEnk <Ricardoenc30@gmail.com>
2023-11-27 01:10:10 -0700

    Modulo de Vehiculos

--- renta-autos/resources/views/vehiculos.blade.php
+++ renta-autos/resources/views/vehiculos.blade.php
@@ -201,21 +201,9 @@
                     <div class="dropdown-divider"></div>
                   </li>
                   <li>
-                    <a class="dropdown-item" href="pages-profile-user.html">
-                      <i class="ti ti-user-check me-2 ti-sm"></i>
-                      <span class="align-middle">My Profile</span>
-                    </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="pages-account-settings-account.html">
-                      <i class="ti ti-settings me-2 ti-sm"></i>
-                      <span class="align-middle">Settings</span>
-                    </a>
-                  </li>
-                  <li>
                     <a class="dropdown-item" href="auth-login-cover.html" target="_blank">
                       <i class="ti ti-logout me-2 ti-sm"></i>
-                      <span class="align-middle">Log Out</span>
+                      <span class="align-middle">Cerrar sesión</span>
                     </a>
                   </li>
                 </ul>
@@ -283,10 +271,13 @@
                           </button>
                           <div class="dropdown-menu">
                             <a class="dropdown-item" href="javascript:void(0);"><i
-                                class="menu-icon tf-icons ti ti-file-description"></i> Consultar</a>
-                            <a class="dropdown-item" href="javascript:void(0);"><i class="ti ti-pencil me-2"></i>
+                                class="menu-icon tf-icons ti ti-file-description" data-bs-toggle="modal"
+                                data-bs-target="#consultaVehiculo"></i> Consultar</a>
+                            <a class="dropdown-item" href="javascript:void(0);"><i class="ti ti-pencil me-2 "
+                                data-bs-toggle="modal" data-bs-target="#editarVehiculoModal"></i>
                               Editar</a>
-                            <a class="dropdown-item" href="javascript:void(0);"><i class="ti ti-trash me-2"></i>
+                            <a class="dropdown-item" href="javascript:void(0);"><i class="ti ti-trash me-2"
+                                data-bs-toggle="modal" data-bs-target="#eliminarVehiculoModal"></i>
                               Eliminar</a>
                           </div>
                         </div>
@@ -307,10 +298,13 @@
                           </button>
                           <div class="dropdown-menu">
                             <a class="dropdown-item" href="javascript:void(0);"><i
-                                class="menu-icon tf-icons ti ti-file-description"></i> Consultar</a>
-                            <a class="dropdown-item" href="javascript:void(0);"><i class="ti ti-pencil me-2"></i>
+                                class="menu-icon tf-icons ti ti-file-description" data-bs-toggle="modal"
+                                data-bs-target="#consultaVehiculo"></i> Consultar</a>
+                            <a class="dropdown-item" href="javascript:void(0);"><i class="ti ti-pencil me-2 "
+                                data-bs-toggle="modal" data-bs-target="#editarVehiculoModal"></i>
                               Editar</a>
-                            <a class="dropdown-item" href="javascript:void(0);"><i class="ti ti-trash me-2"></i>
+                            <a class="dropdown-item" href="javascript:void(0);"><i class="ti ti-trash me-2"
+                                data-bs-toggle="modal" data-bs-target="#eliminarVehiculoModal"></i>
                               Eliminar</a>
                           </div>
                         </div>
@@ -331,10 +325,13 @@
                           </button>
                           <div class="dropdown-menu">
                             <a class="dropdown-item" href="javascript:void(0);"><i
-                                class="menu-icon tf-icons ti ti-file-description"></i> Consultar</a>
-                            <a class="dropdown-item" href="javascript:void(0);"><i class="ti ti-pencil me-2"></i>
+                                class="menu-icon tf-icons ti ti-file-description" data-bs-toggle="modal"
+                                data-bs-target="#consultaVehiculo"></i> Consultar</a>
+                            <a class="dropdown-item" href="javascript:void(0);"><i class="ti ti-pencil me-2 "
+                                data-bs-toggle="modal" data-bs-target="#editarVehiculoModal"></i>
                               Editar</a>
-                            <a class="dropdown-item" href="javascript:void(0);"><i class="ti ti-trash me-2"></i>
+                            <a class="dropdown-item" href="javascript:void(0);"><i class="ti ti-trash me-2"
+                                data-bs-toggle="modal" data-bs-target="#eliminarVehiculoModal"></i>
                               Eliminar</a>
                           </div>
                         </div>
@@ -352,7 +349,7 @@
     </div>
   </div>
 
-  <!-- Modal -->
+  <!-- Modal-Register -->
   <div class="modal fade" id="agregarVehiculo" tabindex="-1" role="dialog" aria-labelledby="form-vehiculo"
     aria-hidden="true">
     <div class="modal-dialog" role="document">
@@ -411,12 +408,144 @@
       </div>
     </div>
   </div>
+  <!--Modal-consulta -->
+  <div class="modal fade" id="consultaVehiculo" tabindex="-1" role="dialog" aria-labelledby="consulta-vehiculo"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="consulta-vehiculo">Detalles del Vehículo</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <!-- Contenido del modal con los datos de la tabla -->
+          <img src="imagen2.jpg" alt="Imagen 2" width="50">
+          <p>Placa: XYZ789</p>
+          <p>Modelo: Accord</p>
+          <p>Marca: Honda</p>
+          <p>Tipo: Sedán</p>
+          <p>Descripción: Descripcion</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Modal de eliminación -->
+  <div class="modal fade" id="eliminarVehiculoModal" tabindex="-1" role="dialog" aria-labelledby="eliminarVehiculoLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="eliminarVehiculoLabel">Eliminar Vehículo</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <p>¿Estás seguro de que deseas eliminar este vehículo?</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+          <!-- Botón para confirmar la eliminación -->
+          <button type="button" class="btn btn-danger" onclick="mostrarMensaje()">Eliminar</button>
+        </div>
+      </div>
+    </div>
+  </div>
 
+  <!-- Modal de mensaje de eliminación exitosa -->
+  <div class="modal fade" id="mensajeEliminacionModal" tabindex="-1" role="dialog"
+    aria-labelledby="mensajeEliminacionLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="mensajeEliminacionLabel">Vehículo Eliminado</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <p>El vehículo se ha eliminado exitosamente.</p>
+        </div>
+      </div>
+    </div>
+  </div>
   <div class="layout-overlay layout-menu-toggle"></div>
   <div class="drag-target"></div>
   </div>
+  <!-- Modal de edición -->
+  <div class="modal fade" id="editarVehiculoModal" tabindex="-1" role="dialog" aria-labelledby="form-vehiculo"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="form-vehiculo">Editar Vehículo</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <form class="card-body">
+            <div class="row mb-3">
+              <label class="col-sm-3 col-form-label text-sm-end">Imagen</label>
+              <div class="col-sm-9">
+                <input type="file" id="vehiculo-imagen" class="form-control" />
+              </div>
+            </div>
+            <div class="row mb-3">
+              <label class="col-sm-3 col-form-label text-sm-end">Placa</label>
+              <div class="col-sm-9">
+                <input type="text" id="vehiculo-placa" class="form-control" placeholder="Placa del Vehículo"
+                  value="XYZ789" />
+              </div>
+            </div>
+            <div class="row mb-3">
+              <label class="col-sm-3 col-form-label text-sm-end">Modelo</label>
+              <div class="col-sm-9">
+                <input type="text" id="vehiculo-modelo" class="form-control" placeholder="Modelo del Vehículo"
+                  value="Accord" />
+              </div>
+            </div>
+            <div class="row mb-3">
+              <label class="col-sm-3 col-form-label text-sm-end">Marca</label>
+              <div class="col-sm-9">
+                <input type="text" id="vehiculo-marca" class="form-control" placeholder="Marca del Vehículo"
+                  value="Honda" />
+              </div>
+            </div>
+            <div class="row mb-3">
+              <label class="col-sm-3 col-form-label text-sm-end">Categoría</label>
+              <div class="col-sm-9">
+                <input type="text" id="vehiculo-categoria" class="form-control" placeholder="Categoría del Vehículo"
+                  value="Sedán" />
+              </div>
+            </div>
+            <div class="row mb-3">
+              <label class="col-sm-3 col-form-label text-sm-end">Estado</label>
+              <div class="col-sm-9">
+                <div class="form-check form-switch">
+                  <input class="form-check-input" type="checkbox" id="vehiculo-estado-disponible" checked>
+                  <label class="form-check-label" for="vehiculo-estado-disponible">Disponible</label>
+                </div>
+              </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+              <button type="button" class="btn btn-success" onclick="guardarCambios()">Guardar Cambios</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
 
-
+  <script>
+    function mostrarMensaje() {
+      $('#eliminarVehiculoModal').modal('hide');
+      $('#mensajeEliminacionModal').modal('show');
+    }
+  </script>
+  <script>
+    function guardarCambios() {
+      $('#editarVehiculoModal').modal('hide');
+    }
+  </script>
   <script src="assets/vendor/libs/jquery/jquery.js"></script>
   <script src="assets/vendor/libs/popper/popper.js"></script>
   <script src="assets/vendor/js/bootstrap.js"></script>
@@ -426,7 +555,6 @@
   <script src="assets/vendor/libs/i18n/i18n.js"></script>
   <script src="assets/vendor/libs/typeahead-js/typeahead.js"></script>
   <script src="assets/vendor/js/menu.js"></script>
-
   <script src="assets/js/main.js"></script>
 </body>
 
